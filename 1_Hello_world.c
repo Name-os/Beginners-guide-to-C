fc76620a90cec57e5621408ca61feab2ba731c5f
Name <jerryjhdong@gmail.com>
2026-04-03 13:21:25 +1300

    testing push and added some start files

---
+++ 1_Hello_world.c
@@ -0,0 +1,12 @@
+// this is the standard library, this has tools to code in C
+#include <stdio.h>
+
+// this is the main function which is run when the program is ran
+int main() {
+
+    // this is print format (stronger print) and prints to the terminal
+    printf("hello world");
+
+    // something for older computers but is not necessary
+    return 0;
+}
