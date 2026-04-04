e76020d45e9adf3d3c60d746edfc7db8d29a3c45
Name <jerryjhdong@gmail.com>
2026-04-04 16:11:11 +1300

    made 2, variables

--- 1_Hello_world.c
+++ 1_Hello_world.c
@@ -1,11 +1,16 @@
+// As tradition of coding, your first program should be printing "Hello world"
+// So I want you to compile and run this file so see what it does
+
+
+
 // this is the standard library, this has tools to code in C
 #include <stdio.h>
 
 // this is the main function which is run when the program is ran
 int main() {
 
-    // this is print format (stronger print) and prints to the terminal
-    printf("hello world");
+    // this is print format (stronger print, don't worry about it for now) and prints to the terminal
+    printf("hello world"); //make sure to add semi-colon, it tells the compiler "this line is done"
 
     // something for older computers but is not necessary
     return 0;
---
+++ 2_variables.c
@@ -0,0 +1,29 @@
+// Variables
+
+// Variables are containers for other values like 1 or 2 or even Hello
+// This allows us to hold a value for later or change a value
+
+// C wants us to specify what type of value we are holding before we even start the program so we have to tell it
+
+
+#include <stdio.h>
+
+int main() {
+
+    // ints, or interger, are just whole numbers and are 32 bit
+    int number = 10; 
+
+    // longs are just longer ints and are 64 bit
+    long longer_number = 100;
+
+    // floats are decimal numbers and are 32 bit
+    float decimal = 10.1;
+
+    // doubles are bigger floats and are 64 bit
+    double longer_decimal = 10.11;
+
+    // chars, or character, hold one single character. Make sure to use single quotes or it will break 
+    char character = 'a';
+
+    return 0;
+}
